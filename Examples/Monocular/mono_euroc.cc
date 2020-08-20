6739c798bc57f520a03f9150f42719d311af6240
zhouwei <zhouwei@migu.com>
2020-08-20 16:56:53 +0800

    update euroc io to simple dataset, surpport csv file load

--- Examples/Monocular/mono_euroc.cc
+++ Examples/Monocular/mono_euroc.cc
@@ -31,58 +31,35 @@ using namespace std;
 
 void LoadImages(const string &strImagePath, const string &strPathTimes,
                 vector<string> &vstrImages, vector<double> &vTimeStamps);
-
+void LoadImagesCSV(const string &strImagePath, const string &strPathTimes,
+                   vector<string> &vstrImages, vector<double> &vTimeStamps);
 int main(int argc, char **argv)
-{  
-    if(argc < 5)
-    {
-        // cerr << endl << "Usage: ./mono_euroc path_to_vocabulary path_to_settings path_to_sequence_folder_1 path_to_times_file_1 (path_to_image_folder_2 path_to_times_file_2 ... path_to_image_folder_N path_to_times_file_N) (trajectory_file_name)" << endl;
-        return 1;
-    }
-
-    const int num_seq = (argc-3)/2;
-    // cout << "num_seq = " << num_seq << endl;
-    bool bFileName= (((argc-3) % 2) == 1);
-    string file_name;
-    if (bFileName)
-    {
-        file_name = string(argv[argc-1]);
-        // cout << "file name: " << file_name << endl;
-    }
+{
 
     // Load all sequences:
     int seq;
-    vector< vector<string> > vstrImageFilenames;
-    vector< vector<double> > vTimestampsCam;
-    vector<int> nImages;
+    vector<string> vstrImageFilenames;
+    vector<double> vTimestampsCam;
+    int nImages;
 
-    vstrImageFilenames.resize(num_seq);
-    vTimestampsCam.resize(num_seq);
-    nImages.resize(num_seq);
+    string datasetPath(argv[3]);
+    string imagePath = datasetPath + "/mav0/cam0/data";
+    string imageFile = datasetPath + "/mav0/cam0/data.csv";
 
-    int tot_images = 0;
-    for (seq = 0; seq<num_seq; seq++)
-    {
-        // cout << "Loading images for sequence " << seq << "...";
-        LoadImages(string(argv[(2*seq)+3]) + "/mav0/cam0/data", string(argv[(2*seq)+4]), vstrImageFilenames[seq], vTimestampsCam[seq]);
-        // cout << "LOADED!" << endl;
+//    LoadImages(imagePath, string(argv[4]), vstrImageFilenames, vTimestampsCam);
+    LoadImagesCSV(imagePath, imageFile, vstrImageFilenames, vTimestampsCam);
+    nImages = vstrImageFilenames.size();
 
-        nImages[seq] = vstrImageFilenames[seq].size();
-        tot_images += nImages[seq];
-    }
 
     // Vector for tracking time statistics
     vector<float> vTimesTrack;
-    vTimesTrack.resize(tot_images);
+    vTimesTrack.resize(nImages);
 
-    // cout << endl << "-------" << endl;
     cout.precision(17);
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::MONOCULAR, true);
 
-    for (seq = 0; seq<num_seq; seq++)
-    {
     #if defined(__APPLE__)
         // Moving main loop to a separate thread so that we could run UI thread on the main thread.
         std::thread runthread([&]() {  // Start in new thread
@@ -90,17 +67,17 @@ int main(int argc, char **argv)
         // Main loop
         cv::Mat im;
         int proccIm = 0;
-        for(int ni=0; ni<nImages[seq]; ni++, proccIm++)
+        for(int ni=0; ni<nImages; ni++, proccIm++)
         {
 
             // Read image from file
-            im = cv::imread(vstrImageFilenames[seq][ni],CV_LOAD_IMAGE_UNCHANGED);
-            double tframe = vTimestampsCam[seq][ni];
+            im = cv::imread(vstrImageFilenames[ni],CV_LOAD_IMAGE_UNCHANGED);
+            double tframe = vTimestampsCam[ni];
 
             if(im.empty())
             {
                 cerr << endl << "Failed to load image at: "
-                     <<  vstrImageFilenames[seq][ni] << endl;
+                     <<  vstrImageFilenames[ni] << endl;
                 return 1;
             }
 
@@ -126,21 +103,15 @@ int main(int argc, char **argv)
 
             // Wait to load the next frame
             double T=0;
-            if(ni<nImages[seq]-1)
-                T = vTimestampsCam[seq][ni+1]-tframe;
+            if(ni<nImages-1)
+                T = vTimestampsCam[ni+1]-tframe;
             else if(ni>0)
-                T = tframe-vTimestampsCam[seq][ni-1];
+                T = tframe-vTimestampsCam[ni-1];
 
             if(ttrack<T)
                 usleep((T-ttrack)*1e6); // 1e6
         }
 
-        if(seq < num_seq - 1)
-        {
-            // cout << "Changing the dataset" << endl;
-
-            SLAM.ChangeDataset();
-        }
     #if defined(__APPLE__)
         // stop viewer so that we could exit UI / main thread
         SLAM.StopViewer();
@@ -150,25 +121,40 @@ int main(int argc, char **argv)
         SLAM.StartViewer();
         runthread.join();
     #endif
-    }
+
     // Stop all threads
     SLAM.Shutdown();
 
-    // Save camera trajectory
-//    if (bFileName)
-//    {
-//        const string kf_file =  "kf_" + string(argv[argc-1]) + ".txt";
-//        const string f_file =  "f_" + string(argv[argc-1]) + ".txt";
-////        SLAM.SaveTrajectoryEuRoC(f_file);
-////        SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
-//    }
-//    else
-//    {
-////        SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
-////        SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
-//    }
-
     return 0;
+}
+//zhouwei add csv file load
+void LoadImagesCSV(const string &strImagePath, const string &strPathTimes,
+                   vector<string> &vstrImages, vector<double> &vTimeStamps)
+{
+    ifstream fTimes;
+    fTimes.open(strPathTimes.c_str());
+    vTimeStamps.reserve(5000);
+    vstrImages.reserve(5000);
+    while(!fTimes.eof())
+    {
+        string s;
+        getline(fTimes,s);
+        if (s[0] == '#')
+            continue;
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double t;
+            char quart;
+            string sRGB;
+            ss>>t;
+            ss>>quart;
+            ss>>sRGB;
+            vstrImages.push_back(strImagePath + "/" +sRGB);
+            vTimeStamps.push_back(t/1e9);
+        }
+    }
 }
 
 void LoadImages(const string &strImagePath, const string &strPathTimes,
